51f962ce7e77a99f01d8029ba8157733167c7f2f
HO-COOH <szpeter@yeah.net>
2021-04-30 08:06:32 +0800

    Add functionality for retrieving all system fonts

--- src/features.ts
+++ src/features.ts
@@ -1,13 +1,42 @@
 "use strict";
-import { QuickPickItem, window } from "vscode";
+import { QuickPickItem, window, WorkspaceConfiguration } from "vscode";
 import { getConfig, parseFontString, Target } from "./utilities";
+import { getFonts } from 'font-list';
 
 interface FontMenuItem extends QuickPickItem {
     type: "font" | "button";
 }
 
+// Reorder the array so that item becomes the first element
+function reorderArray<T>(item: T, arr: T[]) {
+    const index = arr.indexOf(item);
+    if (index !== -1) {
+        arr.splice(index, 1);
+        arr.splice(0, 0, item);
+    }
+    else {
+        arr.unshift(item);
+    }
+}
+
+// Restore the original font setting
+function restoreFonts(targetConfig: WorkspaceConfiguration) {
+    targetConfig.update("fontFamily", targetConfig.fontFamily, true);
+}
+
+// Strip out any single quote and double quote in the string
+function removeQuote(str: string): string {
+    return str.replace(/\"/g, "").replace(/\'/g, "");
+}
+
+// Strip out any single quote and double quote in the font array
+export async function preprocessFontString(target: Target): Promise<void> {
+    await getConfig(target).update("fontFamily", removeQuote(getConfig(target).fontFamily), true);
+}
+
 // Show a quick pick menu for selecting font
 export async function selectFont(target: Target): Promise<void> {
+    await preprocessFontString(target);
     const targetConfig = getConfig(target);
     const existingFonts = parseFontString(targetConfig.fontFamily).filter(x => !!x);
 
@@ -33,13 +62,13 @@ export async function selectFont(target: Target): Promise<void> {
 
     // User cancelled, so apply the original settings
     if (!selection) {
-        targetConfig.update("fontFamily", targetConfig.fontFamily, true);
+        restoreFonts(targetConfig);
         return;
     }
 
     if (selection.type === "button") {
         // Reset to default when user selects a button.
-        targetConfig.update("fontFamily", targetConfig.fontFamily, true);
+        restoreFonts(targetConfig);
 
         if (selection.label === "$(add) Add Font") {
             await addFont(target, existingFonts);
@@ -49,12 +78,9 @@ export async function selectFont(target: Target): Promise<void> {
         return;
     }
 
-    const index = existingFonts.indexOf(selection.label);
-    if (index !== -1) {
-        existingFonts.splice(index, 1);
-        existingFonts.splice(0, 0, selection.label);
-        targetConfig.update("fontFamily", existingFonts.join(", "), true);
-    }
+    // Update the fonts setting, so selection becomes the first font in the array
+    reorderArray(selection.label, existingFonts);
+    targetConfig.update("fontFamily", existingFonts.join(", "), true);
 }
 
 
@@ -80,9 +106,23 @@ export async function selectFontSize(target: Target): Promise<void> {
 
 // Add a font to the fontFamily and set it as active.
 export async function addFont(target: Target, fonts: string[]): Promise<void> {
-    const fontFamily = await window.showInputBox({ placeHolder: "Font Name" });
+    const targetConfig = getConfig(target);
+    const fontFamily = await window.showQuickPick(await getFonts(), {
+        onDidSelectItem: (selection) => {
+            targetConfig.update(
+                "fontFamily",
+                selection,
+                true
+            );
+        }
+    });
     if (fontFamily) {
-        getConfig(target).update("fontFamily", [fontFamily, ...fonts].join(", "), true);
+        const fontStripped = removeQuote(fontFamily);
+        reorderArray(fontStripped, fonts);
+        getConfig(target).update("fontFamily", fonts.join(", "), true);
+    }
+    else { // User cancelled, so restore the original
+        restoreFonts(targetConfig);
     }
 }
 
